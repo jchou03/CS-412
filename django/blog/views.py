5a86a6a815324a9c8c1529645a5f31801ef0d9ef
jchou21 <jared.chou7@gmail.com>
2024-10-08 10:40:00 -0400

    updating model + one to many relationship

--- django/blog/views.py
+++ django/blog/views.py
@@ -3,7 +3,8 @@
 from django.shortcuts import render
 
 from . models import *
-from django.views.generic import ListView
+from django.views.generic import *
+import random
 
 # Create your views here.
 class ShowAllView(ListView):
@@ -13,3 +14,27 @@ class ShowAllView(ListView):
     context_object_name = "articles"
     
     
+class RandomArticleView(DetailView):
+    '''a view to show one article at random'''
+    model = Article
+    template_name = "blog/article.html"
+    context_object_name = "article" 
+        # singular name
+        
+    # AttributeError: Generic detail view RandomArticleView must be called with either an object pk or a slug in the URLconf.
+    # one solution: implement get_object method
+    def get_object(self):
+        '''return the instancec of the Article object to show'''
+        # get all articles
+        all_articles = Article.objects.all()
+            # SELECT *
+        # pick one at random
+        return random.choice(all_articles)
+
+    
+class ArticleView(DetailView):
+    '''a view to show one specific article'''
+    model = Article
+    template_name = "blog/article.html"
+    context_object_name = "article" 
+        # singular name
