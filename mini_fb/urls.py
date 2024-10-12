d5dbb0fd52a71d9683a131e148b83885ff23d9fe
jchou21 <jared.chou7@gmail.com>
2024-10-11 21:01:49 -0400

    task 1 of hw 6

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -7,5 +7,6 @@ from . import views
 
 urlpatterns = [
     path(r'', views.ShowAllProfilesView.as_view(), name="show_all_profiles"),
-    path(r'home', views.ShowAllProfilesView.as_view(), name="show_all_profiles")
+    path(r'home', views.ShowAllProfilesView.as_view(), name="show_all_profiles"),
+    path(r'profile/<int:pk>', views.ShowProfilePageView.as_view(), name="show_profile")
 ]
