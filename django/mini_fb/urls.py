f47f9787d734a77eec18128c10d8fa59faad93da
jchou21 <jared.chou7@gmail.com>
2024-10-14 19:16:39 -0400

    added functionality to add new status messages and display new statuses on profile page

--- django/mini_fb/urls.py
+++ django/mini_fb/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path(r'', views.ShowAllProfilesView.as_view(), name="show_all_profiles"),
     path(r'profile/<int:pk>', views.ShowProfilePageView.as_view(), name="show_profile"),
     path(r'create_profile', views.CreateProfileView.as_view(), name="create_profile"), # goes first, no primary key
+    path(r'profile/<int:pk>/create_status', views.CreateStatusMessageView.as_view(), name="create_status"),
 ]
