a45f51cb03142041b14794d4c1d6378814ee808b
jchou21 <jared.chou7@gmail.com>
2024-10-18 17:51:50 -0400

    added functionality to delete status messages from a profile

--- django/mini_fb/urls.py
+++ django/mini_fb/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path(r'create_profile', views.CreateProfileView.as_view(), name="create_profile"), # goes first, no primary key
     path(r'profile/<int:pk>/create_status', views.CreateStatusMessageView.as_view(), name="create_status"),
     path(r'profile/<int:pk>/update', views.UpdateProfileView.as_view(), name="update_profile"),
+    path(r'status/<int:pk>/delete', views.DeleteStatusMessageView.as_view(), name="delete_status")
 ]
