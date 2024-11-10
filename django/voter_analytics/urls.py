2335e7feb4fb8d6813039fbc9e9f6a6d6dc7afcd
jchou21 <jared.chou7@gmail.com>
2024-11-10 15:54:15 -0500

    added graphs

--- django/voter_analytics/urls.py
+++ django/voter_analytics/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path(r'', views.VoterRecordsView.as_view(), name="voters"),
-    path(r'voter/<int:pk>', views.VoterRecordView.as_view(), name="voter")
+    path(r'voter/<int:pk>', views.VoterRecordView.as_view(), name="voter"),
+    path(r'graphs', views.VoterGraphsView.as_view(), name="graphs")
 ]
--- django/voter_analytics/views.py
+++ django/voter_analytics/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import *
 from .models import *
+import plotly
+import plotly.graph_objs as go
 
 # Create your views here.
 class VoterRecordsView(ListView):
@@ -57,4 +59,51 @@ class VoterRecordView(DetailView):
         context = super().get_context_data(**kwargs)
         context['fields'] = Voter._meta.fields
         print(context['fields'])
+        return context
+    
+class VoterGraphsView(ListView):
+    '''display graphs about aggregate voter data'''
+    
+    model=Voter
+    template_name="voter_analytics/graphs.html"
+
+    def get_context_data(self, **kwargs):
+        '''generate graphs and add to context data'''
+        context = super().get_context_data(**kwargs)
+        
+        # count number of voters for each year
+        years = []
+        party_counts = {}
+        election_participation = []
+        for v in Voter.objects.all():
+            # count birth years
+            years.append(v.birth_date.year)
+            # count parties
+            if v.party in party_counts:
+                party_counts[v.party] += 1
+            else:
+                party_counts[v.party] = 1
+            # count election participation
+            elections = ["v20state", 'v21town', 'v21primary', 'v22general', 'v23town']
+            for e in elections:
+                if getattr(v, e):
+                    election_participation.append(e)
+        
+        # fig = go.Histogram(x=years)
+        # title_text = f"Voters by Birth Year"
+        # graph_voter_birth_years = plotly.offline.plot({"data":[fig], "layout_title_text": title_text}, auto_open=False, output_type="div")
+        # context['voter_birth_years'] = graph_voter_birth_years
+
+        # distribution of voters by party
+        # fig = go.Pie(values=list(party_counts.values()), labels=list(party_counts.keys()))
+        # title_text = f"Voters by Party"
+        # graph_voter_parties = plotly.offline.plot({"data":[fig], "layout_title_text": title_text}, auto_open=False, output_type="div")
+        # context['voter_parties'] = graph_voter_parties
+        
+        # distribution of voter participation across elections
+        fig = go.Histogram(x=election_participation)
+        title_text = f"Voters by Election"
+        graph_election_participation = plotly.offline.plot({"data":[fig], "layout_title_text": title_text}, auto_open=False, output_type="div")
+        context['election_participation'] = graph_election_participation
+        
         return context
