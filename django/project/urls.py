ccf8c5ef695dd2c04a47fbac7d3da5667c374920
jchou21 <jared.chou7@gmail.com>
2024-11-20 21:40:19 -0500

    created basic views to view existing trips and navigate between pages

---
+++ django/project/urls.py
@@ -0,0 +1,12 @@
+# project/urls.py
+# description: app specific URLS for the project application
+
+from django.urls import path
+from django.conf import settings
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path(r'', views.ShowAllTripsView.as_view(), name="show_all_trips"),
+    path(r'trip/<int:pk>', views.ShowTripView.as_view(), name="show_trip"),
+]
